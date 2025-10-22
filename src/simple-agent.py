e506f2bfb4b78414d43a9dd2a79084f3bed9409a
David Uebelacker <david@uebelacker.ch>
2025-10-22 16:55:48 +0200

    :beers: For the fashion of Minas Tirith was such that it was built on seven levels, each delved into a hill, and about each was set a wall, and in each wall was a gate. 		-- J.R.R. Tolkien, "The Return of the King"
    	[Quoted in "VMS Internals and Data Structures", V4.4, when
    	 referring to system overview.]

--- src/simple-agent.py
+++ src/simple-agent.py
@@ -2,6 +2,7 @@ from langgraph.prebuilt import create_react_agent
 from langchain_community.agent_toolkits.openapi.toolkit import RequestsToolkit
 from langchain_community.utilities.requests import TextRequestsWrapper
 from helpers.response_callback import ResponseCallback
+from langchain_ollama.llms import OllamaLLM
 
 tools = RequestsToolkit(
     requests_wrapper=TextRequestsWrapper(headers={}),
@@ -15,7 +16,7 @@ You will help me find vulnerabilities in web applications.
 """
 
 agent = create_react_agent(
-    model="anthropic:claude-sonnet-4-20250514",
+    model=OllamaLLM(base_url="http://192.168.222.220:11434", model="phi4:latest"),
     tools=tools,
     prompt=SYSTEM_PROMPT,
 )
